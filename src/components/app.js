0efe2c2bc753455db228aa6b6ce454fc0fc97459
apupyshev <alexeyalexeevichpupyshev@gmail.com>
2019-05-17 04:04:55 +0300

    added changes

--- src/components/app.js
+++ src/components/app.js
@@ -5,11 +5,27 @@ class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {}
+        this.authFunc = this.authFunc.bind(this);
+    }
+    authFunc() {
+        const authData = { data: "Auth on my site" };
+        if (WavesKeeper) {
+            WavesKeeper.auth( authData )
+            .then(auth => {
+                console.log( auth ); //displaying the result on the console
+                /*...processing data */
+            }).catch(error => {
+                console.error( error ); // displaying the result on the console
+                /*...processing errors */
+            })
+        } else {
+            alert("To Auth WavesKeeper should be installed.");
+        }
     }
     render() {
         return (
             <div className="container">
-    		    <input className="btn btn-primary" type="submit" value="Alert" onClick={() => {alert("Alert button onClick");}}/>
+    		    <input className="btn btn-primary" type="submit" value="Alert" onClick={this.authFunc}/>
     	    </div>
         )
     }
